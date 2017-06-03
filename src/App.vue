dc8313c161c91907c4e4019157aa2d8adf3af91b
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-06-03 19:16:09 +0200

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="container">
+    <main>
+      <component :is="selected">
+        <svg class="winebottle" aria.labelledy="title" xmlns="http://www.w3.org/2000/svg" viewBox="0 155 140 300">
+
+        </svg>
+      </component>
+    </main>
+    <aside>
+      <h4>Name your wine</h4>
+      <input v-model="label" maxlength="18" />
+      <div class="button-row">
+        <h4>Color</h4>
+        <button @click="selected ='appBlack', labelColor = '#000000'">Black Label</button>
+        <button @click="selected ='appWhite', labelColor = '#ffffff'">White Label</button>
+        <input type="color" v-model="labelColor" defaultValue="#ff0000">
+      </div>
+    </aside>
   </div>
 </template>
 
 <script>
+import Black from './components/Black.vue'
+import White from './components/White.vue'
+
 export default {
-  name: 'app'
+  data: function () {
+    return {
+      selected: 'appBlack',
+      label: 'Label Name'
+    }
+  },
+  name: 'app',
+  components: {
+    appBlack: Black,
+    appWhite: White
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/style.css";
 </style>
---
+++ src/components/Black.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.label {
+    fill: black;
+  }
+  .bottle, .wine-text {
+    fill: white;
+  }
+  .flor {
+    fill: #ccc;
+  }
+  .bkimg {
+    filter:url(#inverse)
+  }
+</style>
